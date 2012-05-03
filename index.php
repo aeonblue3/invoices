4a45a8fe020f0627fe4a4def00c8c72d128436a0
Chris Neitzer <chrisn@soholaunch.com>
2012-05-03 10:27:58 -0400

    Revert "Added title bar"
    This reverts commit 1f46f9363912c6299d501e59c480a55852c68395.

--- index.php
+++ index.php
@@ -28,13 +28,7 @@
 </head>
 <body>
 	<div class="container-fluid">
-		<div class="navbar">
-			<div class="navbar-inner">
-				<div class="container">
-					<a class="brand" href="index.php">Invoice Creator</a>
-				</div>
-			</div>
-		</div>
+		<h1>Main Invoice</h1>
 		<div class="row-fluid">
 			<div class="span4">
 			<h2>Invoices</h2>
